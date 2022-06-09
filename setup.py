b3802ad3e999abe1981e5493f75877a52fa0fb11
Alpha Team <alphabestteam@gmail.com>
2022-06-09 09:54:55 +0300

    Changed project's structure.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+   name='bubble_trouble_ai_competition',
+   version='1.0.0',
+   author='Davidalk & Tehila',
+   author_email='elkobyd@gmail.com',
+   packages=['bubble_trouble_ai_competition'],
+   scripts=[],
+   url='http://pypi.python.org/pypi/bubble_trouble_ai_competition/',
+   license='LICENSE.txt',
+   description='An AI Competition based on the game "Bubble Trouble"!',
+   long_description='An AI Competition based on the game "Bubble Trouble"!',
+   install_requires=[
+        "pygame"
+        "sphinx"
+        "sphinx_rtd_theme"
+   ]
+)
