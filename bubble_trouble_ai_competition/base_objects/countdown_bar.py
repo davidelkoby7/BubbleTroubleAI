e71adc09c4a44f8cbc3be1c370ec4606a4402f99
itshadar <hadarber283@gmail.com>
2022-08-17 16:32:29 +0300

    add countdown bar

---
+++ bubble_trouble_ai_competition/base_objects/countdown_bar.py
@@ -0,0 +1,70 @@
+
+from tkinter import S
+import pygame
+from bubble_trouble_ai_competition.base_objects.base_player import BasePlayer
+from bubble_trouble_ai_competition.game_core.events_observable import EventsObservable
+from bubble_trouble_ai_competition.utils.constants import DisplayConstants, Events, Settings, CountdownBarConstans, ScoreboardConstants
+from bubble_trouble_ai_competition.utils.general_utils import load_image_and_keep_aspect_ratio
+
+
+class CountdownBar:
+    """
+    This class represents an arrow shot of a player.
+    """
+    def __init__(self, countdown: int, events_observable: EnvironmentError) -> None:
+        """
+        Initializes the Countdown Bar.
+        
+        Args:
+            countdown (int): The speed of the arrow in the y direction.
+            events_observable (EventsObservable): The events observable.
+        """
+        self.countdown = countdown
+        self.fps = countdown
+        self.x = CountdownBarConstans.BAR_POSITION[0]
+        self.y = CountdownBarConstans.BAR_POSITION[1]
+
+        self.events_observable = events_observable
+
+        # Define the rect inside the progress bar
+        self.rect_image = pygame.Surface((CountdownBarConstans.BAR_WIDTH,
+                        CountdownBarConstans.BAR_HEIGHT)) 
+        
+        self.rect_image.fill((118,238,198))
+        self.countdown_rect = self.rect_image.get_rect(topleft=(self.x, self.y)) 
+        
+       
+
+    def update(self) -> None:
+        """
+        Update countdown, and checks if it has reached the end.
+        notify observers that time is out
+        """
+        self.countdown -= 1
+        if self.countdown == 0:
+            self.events_observable.notify_observers(Events.GAME_TIMEOUT)
+    
+
+
+    def draw(self, screen: pygame.Surface) -> None:
+        """
+        Draws the bar loading on the screen.
+        """
+        # Drawing the background of the countdown bar.
+        pygame.draw.rect(screen, CountdownBarConstans.BAR_COLOR, pygame.Rect(*CountdownBarConstans.BAR_POSITION, CountdownBarConstans.BAR_WIDTH,
+                        CountdownBarConstans.BAR_HEIGHT))
+        
+        # Calculate the new width of the countdown bar (rect)
+        update_countdown_rect_width = self.countdown_rect.w * (self.countdown / self.fps)
+       
+        # Update countdown bar
+        screen.blit(
+            self.rect_image,
+            self.countdown_rect,
+            (0, 0, update_countdown_rect_width, self.countdown_rect.h)
+            )
+        
+     
+
+        
+        
