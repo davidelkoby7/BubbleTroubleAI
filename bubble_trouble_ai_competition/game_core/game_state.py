2b8e0b6327ff4b9ad23f2e70dff43ad13185b96a
itshadar <hadarber283@gmail.com>
2022-09-07 17:37:19 +0300

    added more type_checking

--- bubble_trouble_ai_competition/game_core/game_state.py
+++ bubble_trouble_ai_competition/game_core/game_state.py
@@ -1,21 +1,28 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+
+    # avoiding import cyclic error
+    from bubble_trouble_ai_competition.base_objects.base_player import BasePlayer
+    from bubble_trouble_ai_competition.base_objects.arrow_shot import ArrowShot
+    from bubble_trouble_ai_competition.base_objects.base_ball import Ball
+    from bubble_trouble_ai_competition.base_objects.base_powerup import Powerup
 
 from bubble_trouble_ai_competition.utils.constants import Settings
-from bubble_trouble_ai_competition.base_objects.arrow_shot import ArrowShot
-from bubble_trouble_ai_competition.base_objects.base_ball import Ball
-from bubble_trouble_ai_competition.base_objects.base_powerup import Powerup
+
 
 
 class GameState:
     """ Save the current frame's game state."""
 
-    __ais__ : list[object] = []
-    __shoots__: list[ArrowShot] = []
-    __balls__: list[Ball] = []
-    __powerups__:list[Powerup] = []
+    __ais__ : list['BasePlayer'] = []
+    __shoots__: list['ArrowShot'] = []
+    __balls__: list['Ball'] = []
+    __powerups__:list['Powerup'] = []
     __frames_remaining__ : int = Settings.TOTAL_GAME_FRAMES
 
 
-def update_game_state(ais: list[object], shoots:list[ArrowShot], balls: list[Ball], powerups: list[Powerup], frames_remaining: int) -> None:
+def update_game_state(ais: list['BasePlayer'], shoots:list['ArrowShot'], balls: list['Ball'], powerups: list['Powerup'], frames_remaining: int) -> None:
     GameState.__ais__ = ais
     GameState.__shoots__ = shoots
     GameState.__balls__ = balls
@@ -23,22 +30,22 @@ def update_game_state(ais: list[object], shoots:list[ArrowShot], balls: list[Bal
     GameState.__frames_remaining__ = frames_remaining
 
 
-def game_ais() -> list[object]:
+def game_ais() -> list['BasePlayer']:
     """returns list of ais playing at the current game frame."""
     return GameState.__ais__
 
 
-def game_powerups() -> list[Powerup]:
+def game_powerups() -> list['Powerup']:
     """returns list of the all powerups in game at the current game frame."""
     return GameState.__powerups__
 
 
-def game_shoots() -> list[ArrowShot]:
+def game_shoots() -> list['ArrowShot']:
     """returns list of the active shoots at the current game frame."""
     return GameState.__shoots__
 
 
-def game_balls() -> list[Ball]:
+def game_balls() -> list['Ball']:
     """returns list of the active balls at the current game frame."""
     return GameState.__balls__
 
