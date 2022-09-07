fbfd82d363eb76cdc5c1b6d1c454d884be80aced
itshadar <hadarber283@gmail.com>
2022-09-07 12:29:44 +0300

    add constants image path

--- bubble_trouble_ai_competition/utils/constants.py
+++ bubble_trouble_ai_competition/utils/constants.py
@@ -1,4 +1,3 @@
-
 class BallColors:
     RED = "red"
     BLUE = "blue"
@@ -27,7 +26,13 @@ class Settings:
     """
     All settings for the game (FPS / Title \ ratios etc).
     """
-    
+    BASE_MODULE_DIR = __file__ [:-18] # The minus 18 - to remove the part of the path until the base of our module. TODO: FIX THIS SHIT
+    BASE_AI_DIR = "\\".join(BASE_MODULE_DIR.split("/")[:-1]) + "ais"
+    ASSETS_DIR = BASE_MODULE_DIR + "assets"
+    LEVELS_DIR = BASE_MODULE_DIR + "levels/"
+    BACKGROUND_IMAGE_PATH = ASSETS_DIR + "\\background.jpg"
+    MENU_BACKGROUND_IMAGE_PATH = ASSETS_DIR + "\\MenuBackground.png"
+
     FPS = 60 
     FRAME_TIME = 1 / FPS
     TOTAL_GAME_FRAMES = 20 # Game during in seconds.
@@ -37,6 +42,10 @@ class Settings:
     TITLE = "Bubble Trouble AI Competition"
     BG_COLOR = (0, 0, 0)
 
+    PLAYER_BODY_IMAGE_NAME = "body.png"
+    PLAYER_HEAD_IMAGE_NAME = "head.png"
+    PLAYER_HEAD_LEFT_IMAGE_NAME = "head_left.png"
+    PLAYER_HEAD_RIGHT_IMAGE_NAME = "head_right.png"
     PLAYER_DIMENSIONS = (1.5, 3) # In units of screen bits.
     PLAYER_WIDTH = PLAYER_DIMENSIONS[0] # In units of screen bits.
     PLAYER_HEIGHT = PLAYER_DIMENSIONS[1] # In units of screen bits.
@@ -46,20 +55,24 @@ class Settings:
     PLAYER_SPEED = 20 # In units of screen bits per second.
     HIT_RADIUS = 3 # In units of screen bits.
 
+    BALL_SIZES = [1, 2, 3, 4, 5]
     BALL_SPEED = 10 # In units of screen bits per second.
     DEFAULT_GRAVITY = 25 # In units of screen bits per second^2.
     BALL_SIZE_TO_RADIUS_RATIO = 0.6
     BALL_POPPED_DOWN_SPEED = -20 # In units of screen bits per second.
     BALL_POPPED_UP_SPEED_DEC = 2 # In units of screen bits per second.
+    GREEN_BALL_IMAGE_PATH = ASSETS_DIR + "\\" + "green_ball.png"
+    BLUE_BALL_IMAGE_PATH = ASSETS_DIR + "\\" + "blue_ball.png"
+    RED_BALL_IMAGE_PATH = ASSETS_DIR + "\\" + "red_ball.png"
+    YELLOW_BALL_IMAGE_PATH = ASSETS_DIR + "\\" + "yellow_ball.png"
+    PURPLE_BALL_IMAGE_PATH = ASSETS_DIR + "\\" + "purple_ball.png"
 
+    GREY_ARROW_IMAGE_PATH = ASSETS_DIR + "\\" + "arrow.png"
     ARROW_WIDTH = 0.7 # In units of screen bits.
     ARROW_SPEED = 25 # In units of screen bits per second.
 
-    BASE_MODULE_DIR = __file__ [:-18] # The minus 18 - to remove the part of the path until the base of our module. TODO: FIX THIS SHIT
-    ASSETS_DIR = BASE_MODULE_DIR + "assets"
-    LEVELS_DIR = BASE_MODULE_DIR + "levels/"
-    BACKGROUND_IMAGE_PATH = ASSETS_DIR + "/background.jpg"
-    MENU_BACKGROUND_IMAGE_PATH = ASSETS_DIR + "/MenuBackground.png"
+    POWERUP_WIDTH = 50
+    POWERUP_HEIGHT = 50
 
 settings_properties_to_scale = [
     'PLAYER_DIMENSIONS', 'PLAYER_WIDTH', 'PLAYER_HEIGHT', 'PLAYER_HANDS_SPACING', 'HIT_RADIUS', 'PLAYER_DUCK_HEIGHT', 'HEAD_RADIUS', 'ARROW_WIDTH',
@@ -85,37 +98,54 @@ class DisplayConstants:
 
 
 class PowerupsSettings:
+
+    SPEED_BOOSTER_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "player_speed_boost_powerup.png"
+    FLASH_SUIT_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "flash_suit.png"
+
+    SHIELD_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "shield_powerup.png"
+    SHIELD_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "shield.png"
     SHIELD_SIZE_INCREASE = 1.5 # In units of screen bits.
     SHIELD_WIDTH = Settings.PLAYER_WIDTH + SHIELD_SIZE_INCREASE
     SHIELD_HEIGHT = Settings.PLAYER_HEIGHT + Settings.HEAD_RADIUS*2 + SHIELD_SIZE_INCREASE
 
+    PUNCH_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "punch_collision_powerup.png"
     PUNCH_SPACING = 0.2 # In units of screen bits.
     PUNCH_WIDTH = Settings.PLAYER_WIDTH * 0.6
     PUNCH_HEIGHT = Settings.PLAYER_HEIGHT * 0.8
     
+    PUNCH_ACTION_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "punch.png"
     PUNCH_ACTION_WIDTH = PUNCH_WIDTH * 3
     PUNCH_ACTION_HEIGHT =  PUNCH_HEIGHT
 
-    MUG_WIDTH = Settings.PLAYER_WIDTH + Settings.HEAD_RADIUS*2
-    MUG_HEIGHT = Settings.PLAYER_HEIGHT * 0.6
-    MUG_SPACING = 0.1 # In units of screen bits.
+    SPEED_SLOWER_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "player_speed_slower_powerup.png"
+    MUD_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "mud.png"
+    MUD_WIDTH = Settings.PLAYER_WIDTH + Settings.HEAD_RADIUS*2
+    MUD_HEIGHT = Settings.PLAYER_HEIGHT * 0.6
+    MUD_SPACING = 0.1 # In units of screen bits.
 
+    ICE_CROWN_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "freeze_powerup.png"
     ICE_CROWN_WIDTH = Settings.HEAD_RADIUS * 3
     ICE_CROWN_HEIGHT  = Settings.PLAYER_HEIGHT * 0.6
     ICE_CROWN_SPACING = 0.5 # In units of screen bits.
     ICE_CROWN_Y = DisplayConstants.FLOOR_Y_VALUE - Settings.PLAYER_HEIGHT - Settings.HEAD_RADIUS*2 - ICE_CROWN_HEIGHT + ICE_CROWN_SPACING
 
+    ICE_FREEZE_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "ice_freeze_player.png"
     ICE_FREEZE_WIDTH = Settings.PLAYER_WIDTH + Settings.HEAD_RADIUS*2
     ICE_FREEZE_HEIGHT = Settings.PLAYER_HEIGHT * 0.8
     ICE_FREEZE_Y = DisplayConstants.FLOOR_Y_VALUE - ICE_FREEZE_HEIGHT
 
+    ICE_CUBE_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "ice_cube.png"
     ICE_CUBE_WIDTH = Settings.PLAYER_WIDTH + Settings.HEAD_RADIUS*2
     ICE_CUBE_HEIGHT = (Settings.PLAYER_HEIGHT + Settings.HEAD_RADIUS*2) * 1.3
     ICE_CUBE_SPACING = 0.7 # In units of screen bits.
     ICE_CUBE_Y = DisplayConstants.FLOOR_Y_VALUE - ICE_CUBE_HEIGHT + ICE_CUBE_SPACING
 
-powerup_constants_to_update = [x for x in dir(PowerupsSettings) if ("__" not in x)]
+    RANDOM_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "random_powerup.png"
 
+    DOUBLE_POINTS_POWERUP_IMAGE_PATH = Settings.ASSETS_DIR + "\\" + "double_points_powerup.png"
+    DOUBLE_POINTS_ARROW_IMAGE_PATH = Settings.ASSETS_DIR + "\\" +"double_points_arrow.png"
+
+powerup_constants_to_update = [x for x in dir(PowerupsSettings) if (not x.endswith("IMAGE_PATH") and "__" not in x)]
 class Events:
     """
     All events that can be triggered in the game.
@@ -131,9 +161,11 @@ class Events:
     PLAYER_UPUNCH = "player_up_punch"
     PLAYER_COLLIDES_RPUNCH = "player_collides_left_punch"
     PLAYER_COLLIDES_LPUNCH = "player_collides_right_punch"
+    FREEZE_PLAYER = "freeze_player"
     CHANGE_MENU_TO_GAME = "change_menu_to_game"
     CHANGE_GAME_TO_MENU = "change_game_to_menu"
     QUIT_MENU = "quit_menu"
+
 
 
 ALL_EVENTS_LIST = [getattr(Events, x) for x in dir(Events) if "__" not in x]
